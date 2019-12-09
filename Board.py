0bc6331a077df781cd96a41881c4b91d83cd9f94
msdjr1998 <58569885+msdjr1998@users.noreply.github.com>
2019-12-08 22:17:27 -0500

    Add files via upload

---
+++ Board.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+# src and dst are tuples with the form (y, x)
+class Move:
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
+
+    def __repr__(self):
+        return str(self.src) + "," + str(self.dst)
+
+    def get_distance(self):
+        return (self.dst[0]-self.src[0], self.dst[1]-self.src[1])
+
+
+class Board:
+    def __init__(self):
+        # Black pieces have an even sum of indices
+        # White pieces have an odd sum of indices
+        self.board = np.ones((18, 18))
+
+    def __repr__(self):
+        return str(self.board)
+
+    def is_valid_move(self, move):
+        middle_space = move.src + np.sign(move.getDistance())
+        # Check if the src has a piece, if the destination is empty, and if the spot in between them has a piece
+        if self.board[move.src] == 1 and self.board[move.dst] == 0 and \
+                self.board[(middle_space[0], middle_space[1])] == 1:
+            return True
+        else:
+            return False
+
+    #### TO DO ###
+    def get_valid_moves(self):
+        moves = []
+
+        return moves
+
+    def get_valid_removes(self, player):
+        if player == -1:
+            # We are first, we can remove any one of the following
+            moves = [(0, 0), (0, 17), (17, 0), (17, 17), (8, 8), (8, 9), (9, 8), (9, 9)]
+        else:
+            # We are second, we can only remove pieces adjacent to what our opponent removed
+            col, row = np.where(self.board == 0)
+            col = col[0]
+            row = row[0]
+            moves = []
+            # Collect list of valid pieces we can remove
+            if col - 1 >= 0:
+                moves.append((col - 1, row))
+            if col + 1 <= 17:
+                moves.append((col + 1, row))
+            if row - 1 >= 0:
+                moves.append((col, row - 1))
+            if row + 1 <= 17:
+                moves.append((col, row + 1))
+        return moves
+
+    # Given a tuple (y,x), clear it from the board
+    def update_board_remove(self, piece):
+        self.board[piece] = 0
+
+    # Given a Move(), update the board
+    def update_board_jump(self, move):
+        distance = move.get_distance()
+
+        # Check and handle multi-jump moves, then single jump moves
+        if abs(distance[0]) > 2:
+            for i in range(move.src[0], move.dst[0], np.sign(distance[0])):
+                self.board[(i, move.src[1])] = 0
+        elif abs(distance[1]) > 2:
+            for i in range(move.src[1], move.dst[1], np.sign(distance[1])):
+                self.board[(move.src[0], i)] = 0
+        else:
+            jumped_space = (move.src[0] + np.sign(distance[0]), move.src[1] + np.sign(distance[1]))
+            self.board[jumped_space] = 0
+
+        # Update source and destination spaces
+        self.board[move.src] = 0
+        self.board[move.dst] = 1
---
+++ Player.py
@@ -0,0 +1,101 @@
+import telnetlib
+from Board import *
+
+
+class Player:
+    def __init__(self):
+        # player = 0 -> black, player = 1 -> white
+        # Black pieces have an even sum of indices
+        # White pieces have an odd sum of indices
+        self.player = -1
+        self.board = Board()
+
+    def play_game(self):
+        username = b'1285'
+        password = b'1285'
+        opponent = b'5821'
+        EOL = b'\n'
+
+        tn = telnetlib.Telnet("artemis.engr.uconn.edu", "4705")
+        tn.read_until(b'?Username:')
+        tn.write(username + EOL)
+        tn.read_until(b'?Password:')
+        tn.write(password + EOL)
+        tn.read_until(b'?Opponent:')
+        tn.write(opponent + EOL)
+        print("Successfully logged in.")
+
+        while True:
+            line = tn.read_until(EOL).decode('ascii')
+            print(line)
+
+            if '?Remove:' in line:
+                # Our turn to remove a piece
+                move = self.minimax_remove()
+                self.board.update_board_remove(move)
+                tn.write(move.encode('ascii') + EOL)
+
+            elif 'Removed:' in line:
+                # Update the board when the opponent removes a piece
+                opponent_move = self.move_parser(line)
+                self.board.update_board_remove(opponent_move)
+
+            elif '?Move(' in line:
+                # Our turn to make a move
+                move = self.minimax_jump()
+                self.board.update_board_jump(move)
+                tn.write(move.encode('ascii') + EOL)
+                # Listen for the server to tell us our move
+                tn.read_until(EOL).decode('ascii')
+
+            elif 'Move[' in line:
+                # Update the board when the opponent makes a move
+                opponent_move = self.move_parser(line)
+                self.board.update_board_jump(opponent_move)
+
+            elif 'Color:' in line:
+                color = line[6:]
+                if color == "BLACK":
+                    self.player = 0
+                else:
+                    self.player = 1
+
+            elif 'wins' in line:
+                break
+            elif 'Error' in line or 'Connection to host lost.' in line:
+                print("ERROR:" + line)
+                break
+
+        print('closing connection')
+        tn.close()
+
+    # Parses a move out of a server message
+    # If line has a Remove message, return a tuple
+    # If line has a Moved message, return a Move()
+    def move_parser(self, line):
+        # If there are more than one bracket groups (ex: []:[]), recursively call the function
+        if line.count("]") > 1:
+            target1 = line[line.index("["):line.index("]")+1]
+            target2 = line[line.index("]:[")+2:]
+            return Move(self.move_parser(target1), self.move_parser(target2))
+        else:
+            target_piece = line[line.index("[") + 1:]
+            target_piece = target_piece[0:target_piece.index("]")]
+            return tuple([int(x) for x in target_piece.split(":")])
+
+    #### TO DO ###
+    def minimax_remove(self, alpha=-1, beta=-1, depth=-1):
+        moves = self.board.get_valid_removes(self.player)
+
+        # returns a tuple
+
+    #### TO DO ###
+    def minimax_jump(self, alpha=-1, beta=-1, depth=-1):
+        moves = self.board.get_valid_moves()
+
+        # returns a Move()
+
+
+p = Player()
+#p.play_game()
+
